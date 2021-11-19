a23cc0d2f26bd96859b8aa85df7917eb5c72ef8b
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-11-19 15:09:25 -0600

    Revise to use bash script

---
+++ test-coverage/setup_seeker.sh
@@ -0,0 +1,21 @@
+wget https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/bin/ibm-aspera-connect_4.1.0.46-linux_x86_64.tar.gz
+tar -zxvf ibm-aspera-connect_4.1.0.46-linux_x86_64.tar.gz
+./ibm-aspera-connect_4.1.0.46-linux_x86_64.sh
+rm ibm-aspera-connect*
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+~/miniconda3/bin/conda init bash
+source ~/miniconda3/etc/profile.d/conda.sh
+conda config --set auto_activate_base false
+conda activate
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install mamba -c conda-forge
+mamba install refgenie trim-galore fastqc fastq-screen salmon multiqc
+mkdir -p ~/genomes
+refgenie init -c ~/genomes/genome_config.yaml
+echo 'export REFGENIE="${HOME}/genomes/genome_config.yaml"' >> ${GITHUB_ENV}
+
+refgenie pull mm10/salmon_partial_sa_index --genome-config ~/genomes/genome_config.yaml
