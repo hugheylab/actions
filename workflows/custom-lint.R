5fda042345b7784feb18a661f39c44a085253a96
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-04-06 10:28:59 -0500

    Update workflow and lint script to match lab styling.

--- workflows/custom-lint.R
+++ workflows/custom-lint.R
@@ -1,5 +1,4 @@
 library('data.table')
-library('glue')
 library('lintr')
 library('rex')
 
@@ -8,22 +7,22 @@ if (!exists('repository')) {
   repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
 if (!exists('branch')) branch = 'main'
 
-double_quotes_linter = function(source_file) {
-  content = source_file$full_parsed_content
-  str_idx = which(content$token == 'STR_CONST')
-  squote_matches = which(re_matches(
-    content[str_idx, 'text'],
+doubleQuotesLinter = function(sourceFile) {
+  content = sourceFile$full_parsed_content
+  strIdx = which(content$token == 'STR_CONST')
+  squoteMatches = which(re_matches(
+    content[strIdx, 'text'],
     rex(start, double_quote, any_non_single_quotes, double_quote, end)
   ))
 
   lapply(
-    squote_matches,
+    squoteMatches,
     function(id) {
-      with(content[str_idx[id], ], {
-        line = source_file$file_lines[line1]
+      with(content[strIdx[id], ], {
+        line = sourceFile$file_lines[line1]
         col2 = if (line1 == line2) col2 else nchar(line)
         Lint(
-          filename = source_file$filename,
+          filename = sourceFile$filename,
           line_number = line1,
           column_number = col1,
           type = 'style',
@@ -36,7 +35,7 @@ newDefaults = with_defaults(
   closed_curly_linter = NULL,
   commented_code_linter = NULL,
   cyclocomp_linter = NULL,
-  double_quotes_linter = double_quotes_linter,
+  double_quotes_linter = doubleQuotesLinter,
   line_length_linter(120),
   object_name_linter = object_name_linter('camelCase'),
   open_curly_linter = open_curly_linter(TRUE),
@@ -51,9 +50,9 @@ getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
 
   lfDt = unique(as.data.table(lintsFound), by = c('filename', 'line_number', 'message'))
 
-  lfDt[, lint_link := glue(
-    'https://github.com/hugheylab/{repository}/blob/{branch}/{filename}#L{line_number}',
-    .envir = .SD)]
+  lfDt[, lint_link := sprintf(
+    'https://github.com/hugheylab/%s/blob/%s/%s#L%s',
+    repository, branch, filename, line_number)]
   lfDt[, line := trimws(line)]
   # lfDt[, line := gsub('\\\\r', '', line)]
   # lfDt[, line := gsub('\\\\n', '', line)]
@@ -70,11 +69,10 @@ getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
       warning('There are lines in the exclusion not found in the current code base.')}
     lfDt = lfDt[!lintExclude, on = onCols]}
 
-  # %0D = \r and %0A = \n
-  # Needs different formatting for bash output
-  lfDt[, format_line :=
-         sprintf('%s. %s line %s: %s (%s) \r\n    ```r \r\n    %s  \r\n    ```',
-              .I, filename, line_number, message, lint_link, line)]
+  newlineEsc = ' \r\n'
+  lfDt[, format_line := sprintf(
+    '%s. %s line %s: %s (%s) %s    ```r %s    %s  %s    ```',
+    .I, filename, line_number, message, lint_link, newlineEsc, newlineEsc, line, newlineEsc)]
   return(lfDt)}
 
 getFormattedIssueStr = function(lfDt) {
@@ -103,9 +101,9 @@ getFormattedIssueStr = function(lfDt) {
     onCols = c('filename', 'line_number', 'message', 'line')
     lintExcludeNotFound = lintExclude[!lfDt, on = onCols]
     if (nrow(lintExcludeNotFound) > 0) {
-      lintExcludeNotFound[, format_line :=
-                            sprintf('%s line %s: %s \r\n    ```r \r\n    %s  \r\n    ```',
-                                    filename, line_number, message, line)]
+      lintExcludeNotFound[, format_line := sprintf(
+        '%s line %s: %s \r\n    ```r \r\n    %s  \r\n    ```',
+        filename, line_number, message, line)]
       notFoundStr = paste0(lintExcludeNotFound$format_line, collapse = '\r\n')
       issueStr = paste0(
         issueStr,
