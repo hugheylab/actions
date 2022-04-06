2556ec7f81edd55e222829efdb4aa8c5e933c3bb
Jake Hughey <jakejhughey@gmail.com>
2022-04-06 11:56:10 -0500

    revising lint code.

--- workflows/custom-lint.R
+++ workflows/custom-lint.R
@@ -2,18 +2,14 @@ library('data.table')
 library('lintr')
 library('rex')
 
-if (!exists('repository')) {
-  dirIndexes = gregexpr('/', getwd())[[1]]
-  repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
-if (!exists('branch')) branch = 'main'
+########################################
 
 doubleQuotesLinter = function(sourceFile) {
   content = sourceFile$full_parsed_content
   strIdx = which(content$token == 'STR_CONST')
   squoteMatches = which(re_matches(
     content[strIdx, 'text'],
-    rex(start, double_quote, any_non_single_quotes, double_quote, end)
-  ))
+    rex(start, double_quote, any_non_single_quotes, double_quote, end)))
 
   lapply(
     squoteMatches,
@@ -30,6 +26,81 @@ doubleQuotesLinter = function(sourceFile) {
           line = line,
           ranges = list(c(col1, col2)))})})}
 
+
+getLintIgnore = function(path = 'lint_ignore.csv') {
+  lintIgnore = if (file.exists(path)) {
+    fread(path)
+  } else {
+    data.table(
+      filepath = as.character(), line_number = as.integer(),
+      message = as.character(), line = as.character())}
+  return(lintIgnore)}
+
+
+getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
+  if (is.null(repository)) {
+    dirIndexes = gregexpr('/', getwd())[[1L]]
+    repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1L, nchar(getwd()))}
+
+  if (is.null(branch)) branch = 'main'
+
+  lfDt = unique(as.data.table(lintsFound), by = c('filepath', 'line_number', 'message'))
+
+  lfDt[, lint_link := sprintf(
+    'https://github.com/hugheylab/%s/blob/%s/%s#L%s',
+    repository, branch, filepath, line_number)]
+  lfDt[, line := trimws(line)]
+  setorder(lfDt, filepath, line_number)
+
+  lintIgnore = getLintIgnore()
+  lintIgnoreNotFound = lintIgnore[!lfDt, on = colnames(lintIgnore)]
+  if (nrow(lintIgnoreNotFound) > 0) {
+    warning('lint_ignore.csv contains lines not found in the current code.')}
+  lfDt = lfDt[!lintIgnore, on = colnames(lintIgnore)]
+
+  newlineEsc = ' \r\n'
+  lfDt[, format_line := sprintf(
+    '%s. %s line %s: %s (%s) %s    ```r %s    %s  %s    ```',
+    .I, filepath, line_number, message, lint_link, newlineEsc, newlineEsc, line, newlineEsc)]
+  return(lfDt)}
+
+
+getFormattedIssueStr = function(lfDt) {
+  newlineEsc = ' \r\n'
+  issueStr = paste0(lfDt$format_line, collapse = newlineEsc)
+  fileName = 'temp_csv.csv'
+  fwrite(lfDt[, .(filepath, line_number, message, line)], fileName)
+  tempCsvStr = readChar(fileName, file.info(fileName)$size)
+  unlink(fileName)
+
+  issueStr = paste0(
+    issueStr, newlineEsc,
+    'To have lintr ignore any of these issues, add the appropriate lines of ',
+    'those shown below to lint_ignore.csv at the top-level of the repository:',
+    newlineEsc, '```', newlineEsc, tempCsvStr, newlineEsc, '```')
+  
+  lintIgnore = getLintIgnore()
+  lintIgnoreNotFound = lintIgnore[!lfDt, on = colnames(lintIgnore)]
+
+  if (nrow(lintIgnoreNotFound) > 0) {
+    lintIgnoreNotFound[, format_line := sprintf(
+      '%s line %s: %s \r\n    ```r \r\n    %s  \r\n    ```',
+      filepath, line_number, message, line)]
+    notFoundStr = paste0(lintIgnoreNotFound$format_line, collapse = '\r\n')
+
+    issueStr = sprintf(
+      '%s%sWarning, lint_ignore.csv contains lines not found in the current code: %s%s',
+      issueStr, newlineEsc, newlineEsc, notFoundStr)}
+
+  return(issueStr)}
+
+########################################
+
+if (!exists('repository')) {
+  dirIndexes = gregexpr('/', getwd())[[1]]
+  repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
+if (!exists('branch')) branch = 'main'
+
 newDefaults = with_defaults(
   assignment_linter = NULL,
   closed_curly_linter = NULL,
@@ -41,77 +112,6 @@ newDefaults = with_defaults(
   open_curly_linter = open_curly_linter(TRUE),
   single_quotes_linter = NULL)
 
-getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
-  if (is.null(repository)) {
-    dirIndexes = gregexpr('/', getwd())[[1]]
-    repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
-
-  if (is.null(branch)) branch = 'main'
-
-  lfDt = unique(as.data.table(lintsFound), by = c('filename', 'line_number', 'message'))
-
-  lfDt[, lint_link := sprintf(
-    'https://github.com/hugheylab/%s/blob/%s/%s#L%s',
-    repository, branch, filename, line_number)]
-  lfDt[, line := trimws(line)]
-  # lfDt[, line := gsub('\\\\r', '', line)]
-  # lfDt[, line := gsub('\\\\n', '', line)]
-  # lfDt[, line := gsub('\\r', '', line)]
-  # lfDt[, line := gsub('\\n', '', line)]
-  setorder(lfDt, filename, line_number)
-
-  lintExcludeFilename = 'lint_exclude.csv'
-  if (file.exists(lintExcludeFilename)) {
-    lintExclude = fread(lintExcludeFilename)
-    onCols = c('filename', 'line_number', 'message', 'line')
-    lintExcludeNotFound = lintExclude[!lfDt, on = onCols]
-    if (nrow(lintExcludeNotFound) > 0) {
-      warning('There are lines in the exclusion not found in the current code base.')}
-    lfDt = lfDt[!lintExclude, on = onCols]}
-
-  newlineEsc = ' \r\n'
-  lfDt[, format_line := sprintf(
-    '%s. %s line %s: %s (%s) %s    ```r %s    %s  %s    ```',
-    .I, filename, line_number, message, lint_link, newlineEsc, newlineEsc, line, newlineEsc)]
-  return(lfDt)}
-
-getFormattedIssueStr = function(lfDt) {
-  newlineEsc = ' \r\n'
-  issueStr = paste0(lfDt$format_line, collapse = newlineEsc)
-  # issueStr = gsub('"', '%22', issueStr, fixed = TRUE)
-  # issueStr = gsub("'", "'\"'\"'", issueStr, fixed = TRUE)
-  fileName = 'temp_csv.csv'
-  fwrite(lfDt[, .(filename, line_number, message, line)], fileName)
-  tempCsvStr = readChar(fileName, file.info(fileName)$size)
-  unlink(fileName)
-  issueStr = paste0(
-    issueStr,
-    newlineEsc,
-    'To exclude any of these issues from being detected, add the appropriate ',
-    'lines, shown below, to lint_exclusions.csv at the top-level of the repository:',
-    newlineEsc,
-    '```',
-    newlineEsc,
-    tempCsvStr,
-    newlineEsc,
-    '```')
-  lintExcludeFilename = 'lint_exclude.csv'
-  if (file.exists(lintExcludeFilename)) {
-    lintExclude = fread(lintExcludeFilename)
-    onCols = c('filename', 'line_number', 'message', 'line')
-    lintExcludeNotFound = lintExclude[!lfDt, on = onCols]
-    if (nrow(lintExcludeNotFound) > 0) {
-      lintExcludeNotFound[, format_line := sprintf(
-        '%s line %s: %s \r\n    ```r \r\n    %s  \r\n    ```',
-        filename, line_number, message, line)]
-      notFoundStr = paste0(lintExcludeNotFound$format_line, collapse = '\r\n')
-      issueStr = paste0(
-        issueStr,
-        newlineEsc,
-        'Warning, there are lines in the exclusion not found in the current code base: ',
-        newlineEsc,
-        notFoundStr)}}
-  return(issueStr)}
-
-lintsFound = lint_dir(linters = newDefaults, pattern = rex('.', or(one_of('Rr'), 'Rmd'), end))
+lintsFound = lint_dir(
+  linters = newDefaults, pattern = rex('.', or(one_of('Rr'), 'Rmd'), end))
 lintsFound
