e55543fd9a19b92c4c9ab67b150689977aaf8990
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-06-14 15:06:02 -0500

    Update lint script to more accurately reflect updates

--- workflows/custom-lint.R
+++ workflows/custom-lint.R
@@ -1,86 +1,117 @@
 library('data.table')
-library('glue')
 library('lintr')
 library('rex')
+
+########################################
+
+doubleQuotesLinter = function(sourceFile) {
+  content = sourceFile$full_parsed_content
+  strIdx = which(content$token == 'STR_CONST')
+  squoteMatches = which(re_matches(
+    content[strIdx, 'text'],
+    rex(start, double_quote, any_non_single_quotes, double_quote, end)))
+
+  lapply(
+    squoteMatches,
+    function(id) {
+      with(content[strIdx[id], ], {
+        line = sourceFile$file_lines[line1]
+        col2 = if (line1 == line2) col2 else nchar(line)
+        Lint(
+          filename = sourceFile$filename,
+          line_number = line1,
+          column_number = col1,
+          type = 'style',
+          message = 'Only use single-quotes.',
+          line = line,
+          ranges = list(c(col1, col2)))})})}
+
+
+getLintIgnore = function(path = 'lint_ignore.csv') {
+  lintIgnore = if (file.exists(path)) {
+    fread(path)
+  } else {
+    data.table(
+      filename = as.character(), line_number = as.integer(),
+      message = as.character(), line = as.character())}
+  return(lintIgnore)}
+
+
+getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
+  if (is.null(repository)) {
+    dirIndexes = gregexpr('/', getwd())[[1L]]
+    repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1L, nchar(getwd()))}
+
+  if (is.null(branch)) branch = 'main'
+
+  lfDt = unique(as.data.table(lintsFound), by = c('filename', 'line_number', 'message'))
+
+  lfDt[, lint_link := sprintf(
+    'https://github.com/hugheylab/%s/blob/%s/%s#L%s',
+    repository, branch, filename, line_number)]
+  lfDt[, line := trimws(line)]
+  setorder(lfDt, filename, line_number)
+
+  lintIgnore = getLintIgnore()
+  lintIgnoreNotFound = lintIgnore[!lfDt, on = colnames(lintIgnore)]
+  if (nrow(lintIgnoreNotFound) > 0) {
+    warning('lint_ignore.csv contains lines not found in the current code.')}
+  lfDt = lfDt[!lintIgnore, on = colnames(lintIgnore)]
+
+  newlineEsc = ' \r\n'
+  lfDt[, format_line := sprintf(
+    '%s. %s line %s: %s (%s) %s    ```r %s    %s  %s    ```',
+    .I, filename, line_number, message, lint_link, newlineEsc, newlineEsc, line, newlineEsc)]
+  return(lfDt)}
+
+
+getFormattedIssueStr = function(lfDt) {
+  newlineEsc = ' \r\n'
+  issueStr = paste0(lfDt$format_line, collapse = newlineEsc)
+  fileName = 'temp_csv.csv'
+  fwrite(lfDt[, .(filename, line_number, message, line)], fileName)
+  tempCsvStr = readChar(fileName, file.info(fileName)$size)
+  unlink(fileName)
+
+  issueStr = paste0(
+    issueStr, newlineEsc,
+    'To have lintr ignore any of these issues, add the appropriate lines of ',
+    'those shown below to lint_ignore.csv at the top-level of the repository:',
+    newlineEsc, '```', newlineEsc, tempCsvStr, newlineEsc, '```')
+
+  lintIgnore = getLintIgnore()
+  lintIgnoreNotFound = lintIgnore[!lfDt, on = colnames(lintIgnore)]
+
+  if (nrow(lintIgnoreNotFound) > 0) {
+    lintIgnoreNotFound[, format_line := sprintf(
+      '%s line %s: %s \r\n    ```r \r\n    %s  \r\n    ```',
+      filename, line_number, message, line)]
+    notFoundStr = paste0(lintIgnoreNotFound$format_line, collapse = '\r\n')
+
+    issueStr = sprintf(
+      '%s%sWarning, lint_ignore.csv contains lines not found in the current code: %s%s',
+      issueStr, newlineEsc, newlineEsc, notFoundStr)}
+
+  return(issueStr)}
+
+########################################
 
 if (!exists('repository')) {
   dirIndexes = gregexpr('/', getwd())[[1]]
   repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
 if (!exists('branch')) branch = 'main'
 
-double_quotes_linter = function(source_file) {
-  content = source_file$full_parsed_content
-  str_idx = which(content$token == 'STR_CONST')
-  squote_matches = which(re_matches(
-    content[str_idx, 'text'],
-    rex(start, double_quote, any_non_single_quotes, double_quote, end)
-  ))
-
-  lapply(
-    squote_matches,
-    function(id) {
-      with(content[str_idx[id], ], {
-        line = source_file$file_lines[line1]
-        col2 = if (line1 == line2) col2 else nchar(line)
-        Lint(
-          filename = source_file$filename,
-          line_number = line1,
-          column_number = col1,
-          type = 'style',
-          message = 'Only use single-quotes.',
-          line = line,
-          ranges = list(c(col1, col2))
-        )
-      })
-    }
-  )
-}
-
-newDefaults = with_defaults(
+newDefaults = linters_with_defaults(
   assignment_linter = NULL,
-  closed_curly_linter = NULL,
   commented_code_linter = NULL,
   cyclocomp_linter = NULL,
-  double_quotes_linter = double_quotes_linter,
+  double_quotes_linter = Linter(doubleQuotesLinter),
   line_length_linter(120),
   object_name_linter = object_name_linter('camelCase'),
-  open_curly_linter = open_curly_linter(TRUE),
-  single_quotes_linter = NULL)
+  brace_linter = NULL,
+  single_quotes_linter = NULL,
+  object_usage_linter = NULL)
 
-getLintDt = function(lintsFound, repository = NULL, branch = NULL) {
-  if (is.null(repository)) {
-    dirIndexes = gregexpr('/', getwd())[[1]]
-    repository = substr(getwd(), dirIndexes[length(dirIndexes)] + 1, nchar(getwd()))}
-
-  if (is.null(branch)) branch = 'main'
-
-  lfDt = unique(as.data.table(lintsFound), by = c('filename', 'line_number', 'message'))
-
-  lfDt[, lint_link := glue(
-    'https://github.com/hugheylab/{repository}/blob/{branch}/{filename}#L{line_number}',
-    .envir = .SD)]
-  lfDt[, line := trimws(line)]
-  # lfDt[, line := gsub('\\\\r', '', line)]
-  # lfDt[, line := gsub('\\\\n', '', line)]
-  # lfDt[, line := gsub('\\r', '', line)]
-  # lfDt[, line := gsub('\\n', '', line)]
-  setorder(lfDt, filename, line_number)
-
-  # %0D = \r and %0A = \n
-  # Needs different formatting for bash output
-  lfDt[, format_line :=
-         glue('{.I}. {filename} line {line_number}: {message} ({lint_link}){newLineEsc}    ```r{newLineEsc}    {line}{newLineEsc}    ```',
-              .I = .I, filename = filename, line_number = line_number, message = message, lint_link = lint_link, newLineEsc = ' \r\n', line = line)]
-  return(lfDt)
-}
-
-getFormattedIssueStr = function(lfDt) {
-  newlineEsc = ' \r\n'
-  issueStr = paste0(lfDt$format_line, collapse = newlineEsc)
-  # issueStr = gsub('"', '%22', issueStr, fixed = TRUE)
-  # issueStr = gsub("'", "'\"'\"'", issueStr, fixed = TRUE)
-  return(issueStr)
-}
-
-lintsFound = lint_dir(linters = newDefaults, pattern = rex('.', or(one_of('Rr'), 'Rmd'), end))
+lintsFound = lint_dir(
+  linters = newDefaults, pattern = rex('.', or(one_of('Rr'), 'Rmd'), end))
 lintsFound
